4c7174dfbe197067ba73d2597c32c25a4df178e3
Andra Ditya <radityairawan259@gmail.com>
2026-05-02 01:36:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -223,7 +223,7 @@ elif st.session_state.step in [1, 2, 3]:
     with nav2:
         btn_label = "Lanjut ➡️" if st.session_state.step < 3 else "🚀 KIRIM HASIL"
         if st.button(btn_label):
-            if errors:
+            if False:
                 st.error(f"⚠️ Ada {len(errors)} soal yang belum lengkap pada halaman ini.")
             else:
                 move_step(4 if st.session_state.step == 3 else st.session_state.step + 1); st.rerun()
